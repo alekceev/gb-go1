83146d4755832e9cace33731905cd4135d8c697e
Denis Alexeev <alekceeb@gmail.com>
2021-12-08 09:20:47 +0300

    #5 поправил 2е задание

--- homework5/2_fibo/main.go
+++ homework5/2_fibo/main.go
@@ -11,14 +11,15 @@ func main() {
 	fmt.Print("Введите число: ")
 	fmt.Scanln(&n)
 
-	fibo := map[string]int{"first": 0, "second": 1}
+	cache := map[string]int{"first": 0, "second": 1, "n": 1}
+	fibo(n, cache)
+	fmt.Printf("%d число ряда фибоначи = %d\n", n, cache["second"])
+}
 
-	f := func() int {
-		defer func() { fibo["first"], fibo["second"] = fibo["second"], fibo["first"]+fibo["second"] }()
-		return fibo["first"]
-	}
-
-	for i := 0; i < n; i++ {
-		fmt.Println(f())
+func fibo(n int, cache map[string]int) {
+	if cache["n"] < n {
+		cache["n"]++
+		fibo(n, cache)
+		cache["first"], cache["second"] = cache["second"], cache["first"]+cache["second"]
 	}
 }
