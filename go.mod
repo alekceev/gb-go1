0e821202ddcb0f80b289a2798051b3f89654a864
Denis Alexeev <alekceeb@gmail.com>
2021-12-20 10:13:52 +0300

    #9 config from file

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gb-go1
 
 go 1.17
 
-require github.com/kelseyhightower/envconfig v1.4.0
+require (
+	github.com/kelseyhightower/envconfig v1.4.0
+	gopkg.in/yaml.v2 v2.4.0
+)
--- homework8/main.go
+++ homework8/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gb-go1/homework8/app/config"
 	"log"
 )
 
+var configFile string
+
 func main() {
-	conf, err := config.Get()
+	flag.StringVar(&configFile, "config", "", "path to config")
+	flag.Parse()
+
+	conf, err := config.Get(configFile)
 	if err != nil {
 		log.Fatal(err)
 	}
